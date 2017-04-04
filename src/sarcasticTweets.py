5625bb26b270a5de90fd33c5adafc3f9298571f3
Ryan <schubertr@etown.edu>
2017-04-04 18:03:23 -0400

    improved tweet parsing and started sarcasm detection

--- src/sarcasticTweets.py
+++ src/sarcasticTweets.py
@@ -13,6 +13,16 @@ from twython import Twython
 import nltk
 from nltk import sent_tokenize, word_tokenize, pos_tag
 from sentiment_module import sentiment
+import scipy
+import numpy
+import matplotlib
+import pandas
+import sklearn
+import unicodedata
+import itertools
+
+def remove_all(element, list):
+    return filter(lambda x: x != element, list)
 
 #OAUTH
 
@@ -23,7 +33,7 @@ t = Twython(app_key='UIdeItVBO6TK1HUDFRwmySCNw',
 
 #setup query
 
-query = '#sarcasm'
+query = 'trump'
 hashtag = '#' + query
 
 numOfResults = 100
@@ -34,10 +44,15 @@ search = t.search(q=query, count=numOfResults)
 #init list that will store tweets
 tweets = []
 
+stop_words = open('minimal-stop.txt').read().splitlines()
+
 #parse query results for just the tweet body and store in list.
 for tweet in search['statuses']:
     if tweet['text'].partition(' ')[0] != 'RT' and tweet['text'] != query and tweet['text'] != hashtag: 
         tweets.append(tweet['text'])
+        
+#for tweet in tweets:
+#    print tweet
 
 #init tokens list  
 org_tokens = []
@@ -45,10 +60,25 @@ org_tokens = []
 #tokenize tweets
 for tweet in tweets:
     org_tokens.append(word_tokenize(tweet))
+print "\nbefore rem---------------\n"
 
-#print tokens    
 for t in org_tokens:
     print t
+
+for tweet in org_tokens:
+    for i,t in enumerate(tweet):
+        if t.isalnum() != True:
+            tweet.remove(t)
+
+for t in org_tokens:
+    for sw in stop_words:
+        remove_all(sw, t)
+
+print "\nafter rem---------------\n"
+ 
+for t in org_tokens:
+    print t
+    
     
 print '\n\n-------------------------------------------------------------\n\n'
 
@@ -59,14 +89,26 @@ sentiments = []
 for t in org_tokens:
     sentiments.append(sentiment.sentiment(t).items())
 
-#combine sentiment and token lists 
-stokens = [None]*(len(org_tokens)+len(sentiments))
-stokens[::2] = org_tokens
-stokens[1::2] = sentiments
-
-#print lists
-for t in stokens:
-    print t
+sent_dict = {}
+for i in range(len(tweets)):
+    sent_dict[tweets[i]] = sentiments[i]
+    
+for k,v in sent_dict.items():
+    if v[0][1] < 1 or v[1][1] < 1:
+        del sent_dict[k]
+       
+#print sent_dict.items()[0][0]
+       
+for i,(k,v) in enumerate(sent_dict.items()):
+    sv = v[1][1] - v[0][1]
+    if sv < .5:
+        print "NOT SARCASTIC: " + sent_dict.items()[i][0] + "\n"
+    if sv > .5 and sv < 1:
+        print "PROBABLY NOT SARCASTIC: " + sent_dict.items()[i][0] + "\n"
+    if sv > 1 and sv < 2:
+        print "MAYBE SARCASTIC: " + sent_dict.items()[i][0] + "\n"
+    if sv > 2:
+        print "PROBABLY SARCASTIC: " + sent_dict.items()[i][0] + "\n"
 
 #for key in search['statuses'][0].keys():
  # print(key)
